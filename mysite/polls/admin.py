5f9d75c992256b0ce7c1cf12b369682c11288379
deepengajjar <74157013+deepengajjar@users.noreply.github.com>
2023-12-27 23:27:28 +0530

    new changes

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,5 +4,9 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets  =[(None, {'fields':['question_text']}),
+                 ('Date Info', {'fields':['pub_date']})]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
